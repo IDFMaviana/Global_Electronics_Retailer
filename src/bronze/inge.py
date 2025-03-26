faceefe38d8d4c50ce80ae7a900ee6b03ed3ff93
IDFMaviana <IDFMaviana>
2025-03-26 21:30:20 +0000

    ajuste

--- src/bronze/inge.py
+++ src/bronze/inge.py
@@ -2,8 +2,8 @@ import sys
 sys.path.insert(0,"/Workspace/Repos/git/Global_Electronics_Retailer/src/lib/")
 import ingestors
 
-#config_csv = dbutils.widgets.get("config_csv")
+config_csv = dbutils.widgets.get("config_csv")
 
 ingestors.importfromcsv(config_csv)
 
-#config_raw = dbutils.widgets.get("config_raw")
+config_raw = dbutils.widgets.get("config_raw")
